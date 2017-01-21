ea7a79e49a61270b5994ae5864e533bdf4f5788d
Tarun Singhal <tks@MacBook-Air.local>
2017-01-21 16:51:48 -0500

    files created

---
+++ controller/main.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Main Controller Class
+ */
+
+class MainController extends Common{
+
+    //default function
+    public function __construct(){
+
+      //get the results
+
+      //show template
+      require_once 'views/main.phtml';
+    }
+}
+
+?>
